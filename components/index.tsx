e96f33c31438ec732c3b54b19d4bd8b5a5592fac
Ondřej Bárta <email@ondrejbarta.xyz>
2022-03-24 00:05:25 +0100

    Update index.tsx

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,7 @@
 export * from "./Button";
 export * from "./Checkbox";
 export * from "./Section";
+export * from "./Tabs";
 export * from "./Tag";
 export * from "./Task";
 export * from "./Typography";
