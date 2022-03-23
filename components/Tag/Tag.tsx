98a1c5adb7e30ac7018b1d9fe67d19fdd3ff17e3
Ondřej Bárta <email@ondrejbarta.xyz>
2022-03-24 00:05:19 +0100

    Update Tag.tsx

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { TAB_HEIGHT } from "../Tabs";
 import { Typography } from "../Typography";
 
 export interface TagProps {}
@@ -12,14 +13,15 @@ export const Tag: React.FC<TagProps> = ({ children }) => (
 );
 
 const Border = styled.View`
-  border-radius: 4px;
+  border-radius: 5px;
   border: 1px solid ${({ theme }) => theme.colors.section.separator};
   overflow: hidden;
+  height: ${TAB_HEIGHT}px;
 `;
 
 const Root = styled.View`
   padding: 3px 6px;
-  background: ${({ theme }) => theme.colors.section.background};
+  flex: 1;
 `;
 
 const Label = styled(Typography.Tag.Label)``;
