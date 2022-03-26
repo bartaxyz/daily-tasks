6968305eccfcc571d901d077bdac17a1d0e8bea1
Ondřej Bárta <email@ondrejbarta.xyz>
2022-03-26 22:47:10 +0100

    Update useElectron.tsx

--- utils/useElectron.tsx
+++ utils/useElectron.tsx
@@ -1,7 +1,7 @@
 export const useElectron = () => {
   let electron = null;
 
-  if (typeof window !== "undefined") {
+  if (typeof window !== "undefined" && typeof window.require !== "undefined") {
     electron = window.require("electron");
   }
 
