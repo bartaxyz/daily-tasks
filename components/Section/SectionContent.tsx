c7e3d3c72b4c862d4185fcedb77904f9989ec857
Ondřej Bárta <email@ondrejbarta.xyz>
2022-03-26 02:25:23 +0100

    Update SectionContent.tsx

--- components/Section/SectionContent.tsx
+++ components/Section/SectionContent.tsx
@@ -1,7 +1,8 @@
+import { ViewProps } from "react-native";
 import styled from "styled-components/native";
 
-export interface SectionContentProps {
-  inset: "S" | "M";
+export interface SectionContentProps extends ViewProps {
+  inset: "XXS" | "XS" | "S" | "M";
 }
 
 export const SectionContent: React.FC<SectionContentProps> = ({ ...props }) => (
@@ -9,5 +10,12 @@ export const SectionContent: React.FC<SectionContentProps> = ({ ...props }) => (
 );
 
 const Root = styled.View<SectionContentProps>`
-  padding: ${({ inset }) => (inset === "S" ? "12px" : "24px")};
+  padding: ${({ inset }) =>
+    inset === "XXS"
+      ? "4px"
+      : inset === "XS"
+      ? "8px"
+      : inset === "S"
+      ? "12px"
+      : "24px"};
 `;
