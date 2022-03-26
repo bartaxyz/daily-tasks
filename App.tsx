3ebd698661647743e94ed9bb29175312ac865ece
Ondřej Bárta <email@ondrejbarta.xyz>
2022-03-26 02:25:17 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,34 +1,22 @@
-import { View, StatusBar } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ThemeProvider, useTheme } from "styled-components/native";
+import { ThemeProvider } from "styled-components/native";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
-import { Today } from "./screens/Today";
+import { Navigation } from "./navigation";
 import { darkTheme } from "./theme/dark/theme";
 import { defaultTheme } from "./theme/default/theme";
-
-const Component = () => {
-  const theme = useTheme();
-
-  console.log({ booo: theme });
-  return null;
-};
+import "./firebase";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
-  console.log({ theme: colorScheme === "dark" ? darkTheme : defaultTheme });
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <ThemeProvider theme={colorScheme === "dark" ? darkTheme : defaultTheme}>
-        <Component />
-        <Today />
+        <Navigation colorScheme={colorScheme} />
       </ThemeProvider>
     );
   }
