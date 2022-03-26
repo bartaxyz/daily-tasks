191283d93e53bb7732741c390d92f8a24a96299a
Ondřej Bárta <email@ondrejbarta.xyz>
2022-03-26 02:25:22 +0100

    Update Section.tsx

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { View } from "react-native";
-import styled, { useTheme } from "styled-components/native";
+import { View, ViewProps } from "react-native";
+import styled from "styled-components/native";
 import { SectionContent, SectionContentProps } from "./SectionContent";
 import { SectionSeparator, SectionSeparatorProps } from "./SectionSeparator";
 
-export interface SectionProps {
+export interface SectionProps extends ViewProps {
   separator: "none" | "top" | "bottom" | "vertical" | "around";
   hasBackground?: boolean;
 }
@@ -13,16 +13,13 @@ export const Section: React.FC<SectionProps> & {
   Content: React.FC<SectionContentProps>;
   Separator: React.FC<SectionSeparatorProps>;
 } = (props) => {
-  const theme = useTheme();
   const { separator, hasBackground = true } = props;
 
   const background = hasBackground ? (
-    <Background theme={theme} {...props} />
+    <Background {...props} />
   ) : (
     <View {...props} />
   );
-
-  console.log({ theme });
 
   const content =
     separator === "around" ? (
@@ -33,13 +30,9 @@ export const Section: React.FC<SectionProps> & {
 
   return (
     <Root {...props}>
-      {(separator === "top" || separator === "vertical") && (
-        <Separator theme={theme} />
-      )}
+      {(separator === "top" || separator === "vertical") && <Separator />}
       {content}
-      {(separator === "bottom" || separator === "vertical") && (
-        <Separator theme={theme} />
-      )}
+      {(separator === "bottom" || separator === "vertical") && <Separator />}
     </Root>
   );
 };
@@ -48,14 +41,11 @@ Section.Content = SectionContent;
 Section.Separator = SectionSeparator;
 
 const Root = styled.View<SectionProps>`
-  flex: 1;
   flex-direction: column;
-  align-items: stretch;
   margin: ${({ separator }) => (separator === "around" ? "12px" : 0)};
 `;
 
 const Border = styled.View<SectionProps>`
-  flex: 1;
   border-radius: 10px;
   border: 1px solid ${({ theme }) => theme.colors.section.separator};
   overflow: hidden;
@@ -63,7 +53,6 @@ const Border = styled.View<SectionProps>`
 
 const Background = styled.View<SectionProps>`
   background: ${({ theme }) => theme.colors.background.default};
-  flex: 1;
 `;
 
 const Separator = styled.View`
